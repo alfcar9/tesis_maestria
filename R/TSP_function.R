4c045f0d8caadba5e1c89bc0cd0efba2496beb04
Alfredo Carrillo <acarrillo@inmegen.edu.mx>
2019-10-08 04:57:19 -0500

    crossing con break

--- R/TSP_function.R
+++ R/TSP_function.R
@@ -1,24 +1,31 @@
-library(tidyverse)
-library(gridExtra)
-library(stringr)
+# Load libraries
+library(tidyverse) # Operations like %in%, etc.
+library(gridExtra) # For ploting
+library(stringr) # R for regular expressions
 
-# Plots a TSP solution.
-TSP_plot <- function(cities_pos_df, camino_heuristica, camino_heuristica_original){
+##################################################################
+################## FUNCTIONS #####################################
+##################################################################
+# Function for plotting solutions.
+# Takes as input the dataframe with the coordinates of the cities and two cicles for comparisson. 
+# The first argument path_heuristic is the solution of the algorithm, the current solution.
+# The second argument, path_heuristic_original, is the solution before the swapping procedure.
+TSP_plot <- function(cities_pos_df, path_heuristic, path_heuristic_original){
   n <- nrow(cities_pos_df)
-  g <- ggplot(cities_pos_df, aes(x = V1, y = V2)) + geom_point() + theme_bw() #+ geom_text(aes(label=c(1:n)), hjust=0, vjust=0)
-  
-  if(all(!camino_heuristica==0)){
+  g <- ggplot(cities_pos_df, aes(x = V1, y = V2)) + geom_point() + theme_bw() + geom_text(aes(label=c(1:n)), hjust=0, vjust=0)
+  # Allows plotting if the whole cicle is given or if the cicle is not finished. The second case is mainly for debugging purposes. 
+  if(all(!path_heuristic==0)){
     m <- n
   }
   else{
-    m <- min(which(camino_heuristica==0))-2 
+    m <- min(which(path_heuristic==0))-2 
   }
-  
+  # Detects which edges appear in the original heuristic so they are plotted in black. Otherwise are plotting in red.
   for(i in 1:m){
-    edge <- c(camino_heuristica[i], camino_heuristica[i+1])
+    edge <- c(path_heuristic[i], path_heuristic[i+1])
     i_vec <- flatten_dbl(cities_pos_df[edge[1],])
     j_vec <- flatten_dbl(cities_pos_df[edge[2],])
-    indexes <- which(camino_heuristica_original %in% edge) %>% sort()
+    indexes <- which(path_heuristic_original %in% edge) %>% sort()
     
     if( abs(indexes[2] - indexes[1]) == 1 || (length(indexes) == 3 && indexes[3] - indexes[2] == 1 ) ){
       g <- g + geom_segment(x = i_vec[1], xend = j_vec[1], y = i_vec[2], yend = j_vec[2], color = "black")
@@ -27,21 +34,26 @@ TSP_plot <- function(cities_pos_df, camino_heuristica, camino_heuristica_origina
       g <- g + geom_segment(x = i_vec[1] , xend = j_vec[1], y = i_vec[2], yend = j_vec[2], color = "red") 
     }
   }
+  # Returns a ggplot
   return(g)
 }
 
-# Extract number from string
+# Function for extracting number from string. 
+# The names of the instances in TSLIB are given with the number of nodes at the end.
+# The input string in the function of TSP is the directory of the instance. 
+# This function takes only the number at the end of the name of the instance directory.
 numextract <- function(string){ 
   str_extract(string, "\\d+\\..*$") %>% str_extract("\\d+") %>% as.double()
 }
 
-# Define Euclidean distance funcion
+# Function for calculating the euclidean distance where the inputs are vectors.
 dist_euclidean <- function(i, j){
   result <- (i-j)^2 %>% sum() %>% sqrt()
   return(result)
 }
 
-# Given set of coordiantes calculates euclidean distance.
+# Given a matrix of two columnos and n rows, which set the coordinates of cities. This function
+# calculates the matrix of distances.
 matrix_euclidean <- function(matrix_pos, n){
   matrix_dist <- matrix(integer(n^2), ncol = n)
   for(i in 1:(n-1)){
@@ -56,7 +68,10 @@ matrix_euclidean <- function(matrix_pos, n){
 }
 
 # Makes symmetric a matrix by checking if matrix M[i][j] = M[j][i]. 
-# If the values differ then it replaces the smaller equal to the greater value.
+# If the values differ then it replaces the greater by the smaller value.
+# This function is useful after we take only the first kth nearest neighbors but because there is no symmetry 
+# that is, if a is the nearest neighbor of b, not necessarily b is the closest neighbor of a. Thus, we check 
+# if M[i,j] = inifity, but M[j,i] = 100, we make M[i,j] = 100. We add an edge instead of removing one.
 matrix_symmetric <- function(matrix, n){
   for(i in 1:(n-1)){
     for(j in (i+1):n){
@@ -77,6 +92,7 @@ index_function <- function(paths_vec,j){
   return(which(paths_vec==j)[1])
 }
 
+# Detects crossing and replaces them.
 crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, costs_vec, cities_pos, cities_dist_original){
   epsilon <- 1e-4
   city21 <- city_current
@@ -92,7 +108,10 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
     m2 <- Inf
   }
   b2 <- y21-m2*x21
-  for(k in (num_iter-1):1){ # ... las restantes aristas
+  k <- num_iter-1
+  history_cities <- c()
+  while(k >= 1){
+  #for(k in (num_iter-1):1){
     city11 <- paths_vec[k]
     x11 <- cities_pos[city11,1]
     y11 <- cities_pos[city11,2]
@@ -106,6 +125,7 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
       m1 <- Inf
     }
     b1 <- y11 - m1 * x11
+    
     if(m1 < Inf & m2 < Inf){
       if(m1 != m2){
         x_intersect <- -(b2 - b1)/(m2 - m1)
@@ -122,7 +142,8 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
           x12_aux <- max(x11, x12)
           x21_aux <- min(x21, x22)
           x22_aux <- max(x21, x22)
-          bool1 <- (x11_aux + epsilon < x21_aux & x21_aux < x12_aux - epsilon) | (x11_aux + epsilon < x22_aux & x22_aux < x12_aux - epsilon) | (x11_aux == x21_aux) | (x22_aux == x12_aux)
+          bool1 <- (x11_aux + epsilon < x21_aux & x21_aux < x12_aux - epsilon) | (x11_aux + epsilon < x22_aux & x22_aux < x12_aux - epsilon) | (x11_aux == x21_aux) | (x22_aux == x12_aux) |
+            (x22_aux > x12_aux & x21_aux < x11_aux) | (x12_aux > x22_aux & x11_aux < x21_aux)
           bool2 <- TRUE
         }
       }
@@ -138,19 +159,26 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
       y_intersect <- m1 * x_intersect + b1
       bool1 <- (x11 + epsilon < x_intersect & x_intersect < x12 - epsilon ) | (x12 + epsilon < x_intersect & x_intersect < x11 - epsilon)
       bool2 <- (y21 + epsilon < y_intersect & y_intersect < y22 - epsilon ) | (y22 + epsilon < y_intersect & y_intersect < y21 - epsilon)
-    }
+      }
     else{
+      # Checks if two vertical segments of line intersect
+      bool1 <- TRUE
       if(x11 == x21){
         y11_aux <- min(y11, y12)
         y12_aux <- max(y11, y12)
         y21_aux <- min(y21, y22)
         y22_aux <- max(y21, y22)
-        bool1 <- TRUE
-        bool2 <- (y11_aux + epsilon < y21_aux & y21_aux < y12_aux - epsilon) | (y11_aux + epsilon < y22_aux & y22_aux < y12_aux - epsilon) | (y11_aux == y21_aux) | (y22_aux == y12_aux)
+        bool2 <- (y11_aux + epsilon < y21_aux & y21_aux < y12_aux - epsilon) | (y11_aux + epsilon < y22_aux & y22_aux < y12_aux - epsilon) | (y11_aux == y21_aux) | (y22_aux == y12_aux) |
+          (y22_aux > y12_aux & y21_aux < y11_aux) | (y12_aux > y22_aux & y11_aux < y21_aux)
+      }
+      else{
+        bool2 <- FALSE
       }
     }
     if(bool1 & bool2){ # condition for finding crossing
-      if(k != num_iter-1){
+      if(city12 %in% history_cities)
+        break
+      if(k != num_iter-1){ # We distinguish the case when the swap is between the last edge, and all the other edges.
         replace_crossing <- rev(paths_vec[(k+1):num_iter])
         paths_vec[(k+1):num_iter] <- replace_crossing
         dist1_replace <- cities_dist_original[city11,city21]
@@ -158,14 +186,12 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
         costs_vec[k:num_iter] <- c(dist1_replace, rev(costs_vec[(k+1):(num_iter-1)]), dist2_replace)
         num_iter_aux <- index_function(paths_vec, city11)
         if(num_iter_aux > 2){
-          list_crossing <- crossing_procedure(city11, city21, num_iter_aux, i, paths_vec, costs_vec, cities_pos, cities_dist_original)
+          list_crossing <- crossing_procedure(city11, city21, num_iter_aux, i, paths_vec, costs_vec, cities_pos, cities_dist_original) # Recursive crossing
           paths_vec <- list_crossing[[1]]
           costs_vec <- list_crossing[[2]]
         }
         city_current <- paths_vec[num_iter]
         city21 <- city_current
-        x21 <- cities_pos[city21,1]
-        y21 <- cities_pos[city21,2]
       }
       else{
         replace_crossing <- rev(paths_vec[k:num_iter])
@@ -176,18 +202,29 @@ crossing_procedure <- function(city_current, city_min, num_iter, i, paths_vec, c
         city_current <- paths_vec[num_iter]
         city21 <- city_current
       }
+      x21 <- cities_pos[city21,1]
+      y21 <- cities_pos[city21,2]
+      if(x22 - x21 != 0){
+        m2 <- (y22-y21)/(x22-x21)
+      }
+      else{
+        m2 <- Inf
+      }
+      b2 <- y21-m2*x21
+      k <- num_iter-1
+      history_cities <- c(history_cities, city21)
     } # end of if crossing exist
-  } # end of for possible crossing
+    k <- k-1
+  } # end of while possible crossing
   list_crossing <- list(paths_vec, costs_vec)
 }
 
-name_instance <- "a54.tsp"
-TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_index = 1, initial_solution){
+name_instance <- "eil101.tsp"
+TSP_function <- function(proportion_edges = 0.2, immediate_value = 2, position_index = 2, initial_solution){
   name_instance <- paste0("~/Maestria/4to_Semestre/Tesis_Maestria/tesis_maestria/Instancias/", name_instance)
-  n <- numextract(name_instance) %>% as.double()
+  n <- numextract(name_instance)
   instancia <- read_table2(name_instance, skip = 6, col_names = FALSE, n_max = n, col_types = cols())
-  cities_pos <- instancia[,2:3]
-  cities_pos <- as.matrix(cities_pos)
+  cities_pos <- as.matrix(instancia[,2:3])
   colnames(cities_pos) <- c("V1", "V2")
   cities_dist_original <- matrix_euclidean(cities_pos, n) # Generate distance matrix
   cities_dist <- cities_dist_original # For last iteration, save original values
@@ -211,18 +248,18 @@ TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_i
   for(i in 1:n){
     neighbors_list[[i]] <- which(as.logical((cities_dist[i,] < Inf)*(cities_dist[i,] > 0) %>% as.integer()))
   }
-  # Estricturas a usar
+  # Instanciate structures to use throught the algorithm.
   paths <- matrix(integer((n+1)*n), nrow = n) # The Paths starting at each node. In each row we try to form a cycle.
   paths[,c(1,(n+1))] <- 1:n
   costs <- matrix(integer(n^2), nrow = n) # The cost of traveling from [i,j] to [i,j+1]
-  # Vector con numero de neighbors
-  degree_neighbors <- sapply(1:n, function(i){neighbors_list[[i]] %>% length()}) #Number of neighbors of each node
+  # Vector with number of neighbors
+  degree_neighbors <- sapply(1:n, function(i){neighbors_list[[i]] %>% length()}) # Number of neighbors of each node
   degree_neighbors_list <- list() # For each cycle we need to keep control of how many neighbors are left. We define a list.
   for(i in 1:n){
     degree_neighbors_list[[i]] <- degree_neighbors
   }
   
-  # Primero generamos un cycle hamiltoniano para cada nodo.
+  # First we generate a hamiltonian cycle starting at each node.
   m <- n-1 # Si se quiere ver como se ve el camino en la m-ésima iteracion se puede ajustar, sino dejar m = n-1
   for(num_iter in 1:m){
     for(i in 1:n){
@@ -281,11 +318,12 @@ TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_i
   if(m == n-1){
     costs[1:n, n]  <- sapply(1:n, function(i){cities_dist_original[paths[i, n], i]})
   }
-  # Se calcula el costo de cada ciclo
+  # The cost of each cycle is calculated
   costs_cycles <- sapply(1:n, function(i){sum(costs[i,])})
-
+  
+  # In case we do not want to select the best cycle we can choose second or third best.
   costs_cycles_aux <- costs_cycles
-  cycle_indexes <- c()
+  cycle_indexes <- c() 
   for(i in 1:position_index){
     cycle_index <- which.min(costs_cycles_aux)
     cycle_indexes[i] <- cycle_index
@@ -320,9 +358,9 @@ TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_i
   # Se inicializan el camino que ha de swapearse.
   path_min_swap <- path_min_noswap
   cost_min_swap <- cost_min_noswap
-  
+
   # Matriz que a cada fila corresponde un ciclo. Cada celda guarda que numero de visita es cada ciudad. Si en [1,2] = 13, entonces la 13ava ciudad visitada
-  # en el ciclo 1 es la ciudad 2. 
+  # en el ciclo 1 es la ciudad 2.
   index_matrix <- matrix(0L, nrow = n, ncol = n)
   # Se generan los caminos extendidos para poder considerar en una sola matriz todos los caminos posibles leidos como secuencia
   paths_ext <- matrix(0L, nrow = n, ncol = 2*n)
@@ -334,16 +372,16 @@ TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_i
     paths_ext[i,] <-  c(paths[i,], paths[i,2:n])
     costs_ext[i,] <-  c(costs[i,], costs[i, 1:(n-1)])
   }
-  
+
   path_min_swap_ext <- c(path_min_swap, path_min_swap[2:n])
   cost_min_swap_ext <- c(cost_min_swap, cost_min_swap[1:(n-1)])
   if(missing(initial_solution)){
-    index_min_swap <- index_matrix[cycle_index,]  
+    index_min_swap <- index_matrix[cycle_index,]
   }
   else{
     index_min_swap <- sapply(1:n, function(j) index_function(path_min_swap, j))
   }
-  
+
   success <- 1 # Indica que se encontró alguna estructura que swapear, se define 1 para que entre al while al menos una vez
   while(success>0){
     success <- 0  # no se han encontrado estructuras swapeables
@@ -486,20 +524,19 @@ TSP_function <- function(proportion_edges = 0.2, immediate_value = 3, position_i
   return(list_output)
 }
 
-output_tour <- TSP_function(proportion_edges = 0.05, immediate_value = 3, position_index = 2)
+output_tour <- TSP_function(proportion_edges = 0.05, immediate_value = 3, position_index = 1)
 name_tour <- paste0("tour", 1)
 assign(name_tour, output_tour)
-# for(j in 2:15){
-#   output_tour <- TSP_function(proportion_edges = runif(1,1/25,0.7), immediate_value = ceiling(4*runif(1)), initial_solution = output_tour[[1]])
-#   name_tour <- paste0("tour", j)
-#   assign(name_tour, output_tour)
-# }
+for(j in 2:10){
+  output_tour <- TSP_function(proportion_edges = runif(1,1/25,0.7), immediate_value = ceiling(4*runif(1)), initial_solution = output_tour[[1]])
+  name_tour <- paste0("tour", j)
+  assign(name_tour, output_tour)
+}
 
-#tour0 <- c(14, 13, 52, 11, 51, 12, 28, 27, 46, 38, 40, 39, 35, 34, 44, 37, 24, 48, 15,  5,  6, 25,  4, 43, 33, 10,  9, 45, 32, 49, 36,  1, 22, 19,  8, 41, 18, 31, 21,  3, 17, 42,  7,  2, 23, 30, 20, 50, 29, 16, 47, 26, 14)
 tour_plot1 <- tour1
 tour_plot2 <- tour1
-#name_instance_path <- paste0("~/Maestria/4to_Semestre/Tesis_Maestria/tesis_maestria/Instancias/", name_instance)
-#n <- numextract(name_instance) %>% as.double()
+name_instance_path <- paste0("~/Maestria/4to_Semestre/Tesis_Maestria/tesis_maestria/Instancias/", name_instance)
+n <- numextract(name_instance) %>% as.double()
 instancia <- read_table2(name_instance_path, skip = 6, col_names = FALSE, n_max = n, col_types = cols())
 cities_pos <- as.matrix(instancia[,2:3])
 colnames(cities_pos) <- c("V1", "V2")
